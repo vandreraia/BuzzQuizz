59d33b2d47b0f6382620c10ef5c8564062653963
Vandré <vandreraia@gmail.com>
2022-04-20 18:27:53 -0300

    feito conexao com a API, perguntas dos quizz quase terminado

---
+++ script.js
@@ -0,0 +1,83 @@
+const API = "https://mock-api.driven.com.br/api/v6/buzzquizz";
+let test;
+
+function getQuizzes() {
+    const promise = axios.get(`${API}/quizzes`);
+    promise.then(postQuizzes);
+    promise.catch(function () {
+        console.log("getQuizzes error");
+    });
+}
+
+function postQuizzes(response) {
+    const quizzes = response.data;
+    const element = document.querySelector(".todos-quizzes");
+    test = quizzes;
+    //element.innerHTML = "";
+    for (let i = 0; i < quizzes.length; i++) {
+        element.innerHTML += `
+        <div id="${quizzes[i].id}" onclick="showquizz(this.id)" class="quizz">
+            <img src="${quizzes[i].image}" />
+            <div>
+                <p>${quizzes[i].title}</p>
+            </div>
+        </div>`;
+    }
+}
+
+function showquizz(id) {
+    document.querySelector(".quizzez-list").classList.add("hide");
+    document.querySelector(".quizz-page").classList.remove("hide");
+    const promise = axios.get(`${API}/quizzes/${id}`);
+    promise.then(postQuizz);
+    promise.catch(function () {
+        console.log("showquizz error");
+    });
+}
+
+function postQuizz(response) {
+    const quizz = response.data;
+    let el;
+    el = document.querySelector(".titulo");
+    el.innerHTML = `
+    <img src="${quizz.image}" />
+    <div>
+        <p>${quizz.title}</p>
+    </div>`;
+    test = quizz;
+    el = document.querySelector(".question-container");
+    el2 = document.querySelector(".respostas");
+    el.innerHTML = "";
+
+    for (let i = 0; i < quizz.questions.length; i++) {
+        el.innerHTML += `
+        <div style="background-color:${quizz.questions[i].color};" class="pergunta flex-center">
+            ${quizz.questions[i].title}
+        </div>
+        <div class="respostas flex-center">`;
+        quizz.questions[i].answers.sort(randomizador);
+        for (let j = 0; j < quizz.questions[i].answers.length; j++) {
+            el.innerHTML += `
+            <div id="${quizz.questions[i].answers[j].isCorrectAnswer}" class="resposta" onclick="checkResposta(this.id)">
+                <img src="${quizz.questions[i].answers[j].image}" />
+                <p>${quizz.questions[i].answers[j].text}</p>
+            </div>`;
+        }
+        el.innerHTML += `</div>`;
+    }
+}
+
+function checkResposta(id) {
+    console.log(id)
+}
+
+function randomizador() {
+    return Math.random() - 0.5;
+}
+
+function home() {
+    document.querySelector(".quizzez-list").classList.remove("hide");
+    document.querySelector(".quizz-page").classList.add("hide");
+}
+
+getQuizzes();
