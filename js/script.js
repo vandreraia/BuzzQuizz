65aaa3f40cbe3b7321718b10b8448057286ff157
Fernandosousa9 <fernando.castro10.master@gmail.com>
2022-04-21 23:54:20 -0300

    criação das perguntas do quizz

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const API = "https://mock-api.driven.com.br/api/v6/buzzquizz";
 let test;
 let obj = {};
+let qtddLevels;
 
 //Código comentado pois estou reduzindo à uma única função, quando a função estiver pronta removerei
 
@@ -71,10 +72,21 @@ let obj = {};
 //     restricaoNumeros();
 // }
 
-function verificaImage(verurlimg){
+//Avança à tela de criação do quizz
+function criarQuizz(){
+    document.querySelector("main").classList.add("hide");
+    document.querySelector(".info-quizz").classList.remove("hide");
+}
+
+//Verifica se a url é uma imagem válida
+function verificarImage(verurlimg){
     return (verurlimg.match(/\.(jpeg|jpg|gif|png)$/) != null);
 }
 
+//Verifica se a cor passada é válida
+function verificarCor(vercorhex) {
+    return (vercorhex.match(/^#[a-f0-9]{6}$/i) != null);
+}
 
 //Validação dos campos
 function validacao(){
@@ -82,8 +94,9 @@ function validacao(){
     const urlImageQUizz = document.getElementById("url-image").value;
     const numPerguntasQuizz = document.getElementById("num-perguntas").value;
     const niveisQuizz = document.getElementById("num-quizz").value;
+    qtddLevels = niveisQuizz;
 
-    if(tituloQuizz < 20 || tituloQuizz > 65 || numPerguntasQuizz < 3 || niveisQuizz < 2 || !verificaImage(urlImageQUizz)){
+    if(tituloQuizz < 20 || tituloQuizz > 65 || numPerguntasQuizz < 3 || niveisQuizz < 2 || !verificarImage(urlImageQUizz)){
         alert("Peencha os campos corretamente");
     }else{
         obj = {
@@ -103,25 +116,117 @@ function criarPerguntas(numPerguntasQuizz){
     const container = document.querySelector(".build-quizz .container");
     for (let i=1; i<=numPerguntasQuizz; i++){
         container.innerHTML += `
-        <div class=".pergunta"PerguntaDOconteiner">
-        <h3 class=".pergunta">Pergunta ${i}</h3>
-        <input type="text" placeholder="Texto da pergunta" data-identifier="question"/>
-        <input type="text" placeholder="Cor de fundo da pergunta" data-identifier="question"/>
-        <h3 class="Texto-Especifica-Input Texto-Definiçao">Resposta correta</h3>
-        <input type="text" placeholder="Resposta correta" data-identifier="question"/>
-        <input type="text" placeholder="URL da imagem" data-identifier="question"/>
-        
-        <h3 class="">Respostas Incorretas</h3>
-        <input type="text" placeholder="Resposta incorreta 1" data-identifier="question"/>
-        <input type="text" placeholder="URL da imagem 1" data-identifier="question"/>
-        <input type="text" placeholder="Resposta incorreta 2" data-identifier="question"/>
-        <input type="text" placeholder="URL da imagem 2" data-identifier="question"/>
-        <input type="text" placeholder="Resposta incorreta 3" data-identifier="question"/>
-        <input type="text" placeholder="URL da imagem 3" data-identifier="question"/>
+        <div class="info-quizz perguntaContainer container-pergunta">
+            <span class="flex-center"><h3>Pergunta ${i}</h3><ion-icon class="create" onclick="ocultar(this)" name="create-outline"></ion-icon></span>
+            <div class="check hide">
+                <input type="text" placeholder="Texto da pergunta" data-identifier="question"/>
+                <input type="text" placeholder="Cor de fundo da pergunta" data-identifier="question"/>
+                <h3 class="">Resposta correta</h3>
+                <input type="text" placeholder="Resposta correta" data-identifier="question"/>
+                <input type="text" placeholder="URL da imagem" data-identifier="question"/>
+                
+                <h3 class="">Respostas Incorretas</h3>
+                <input class="input" type="text" placeholder="Resposta incorreta 1" data-identifier="question"/>
+                <input class="input" type="text" placeholder="URL da imagem 1" data-identifier="question"/>
+                <input class="input" type="text" placeholder="Resposta incorreta 3" data-identifier="question"/>
+                <input class="input" type="text" placeholder="Resposta incorreta 2" data-identifier="question"/>
+                <input class="input" type="text" placeholder="URL da imagem 2" data-identifier="question"/>
+                <input class="input" type="text" placeholder="URL da imagem 3" data-identifier="question"/>
+            </div>
         </div>`;
     }
 }
 
+//Validação das perguntas do quizz criado
+function validarPerguntas(){
+    const numPerguntas = document.querySelectorAll(".perguntaContainer");
+    let perguntasValidadas = 0;
+
+    for(let i=0; i<numPerguntas.length; i++){
+        let indicePergunta = numPerguntas[i];
+
+        if((indicePergunta.children[1].value).length <c20 || !verificarCor(indicePergunta.children[2].value) || indicePergunta.children[2].value===""
+         || indicePergunta.children[4].value==="" || indicePergunta.children[7].value==="" || 
+        !verificarImage(indicePergunta.children[5].value) || !verificarImage(indicePergunta.children[8].value) || 
+        (indicePergunta.children[9].value!=="" && !verificarImage(indicePergunta.children[10].value)) || 
+        (indicePergunta.children[11].value!=="" && !verificarImage(indicePergunta.children[12].value))){
+            perguntasValidadas = 0;
+            alert("Por favor, preencha os dados corretamente para prosseguir.");
+        } else {
+            perguntasValidadas +=1;
+
+            obj.questions.push ({
+                title: indicePergunta.children[1].value,
+                color: indicePergunta.children[2].value,
+                answers: []
+            });
+            obj.questions[i].answers.push({
+                text: indicePergunta.children[4].value,
+                image: indicePergunta.children[5].value,
+                isCorrectAnswer: true
+            },
+            {
+                text: indicePergunta.children[7].value,
+                image: indicePergunta.children[8].value,
+                isCorrectAnswer: false
+            });
+            if(indicePergunta.children[9].value!==""){
+                obj.questions[i].answers.push({
+                    text: indicePergunta.children[9].value,
+                    image: indicePergunta.children[10].value,
+                    isCorrectAnswer: false
+                });
+            }
+            if(indicePergunta.children[11].value!==""){
+                obj.questions[i].answers.push({
+                    text: indicePergunta.children[11].value,
+                    image: indicePergunta.children[12].value,
+                    isCorrectAnswer: false
+                });
+            }
+        }
+    }
+    if (perguntasValidadas === numPerguntas.length){
+
+        const screenPerguntas = document.querySelector(".build-quizz");
+        const screenNiveis = document.querySelector(".build-levels");
+
+        screenPerguntas.classList.add("hide");
+        screenNiveis.classList.remove("hide");
+        criarNiveis(qtddLevels);
+        perguntasValidadas = 0;
+    } else {
+        obj.questions = [];
+        perguntasValidadas = 0;
+    }
+}
+
+//Valida os niveis do quizz
+function criarNiveis(qtddLevels) {
+    const containerLevels = document.querySelector(".build-levels .container");
+    for (let i=1; i<=qtddLevels; i++){
+        containerLevels.innerHTML += `
+        <div class="levelQuizz container-level input">        
+            <h3>Nível ${i}</h3>
+            <input type="text" placeholder="Título do nível" data-identifier="level"/>
+            <input type="text" class="PorcentagemdeAcerto" placeholder="% de acerto mínima" data-identifier="level"/>
+            <input type="text" placeholder="URL da imagem do nível" data-identifier="level"/>
+            <input type="text" placeholder="Descrição do nível" data-identifier="level"/>
+        </div>`;
+    }
+}
+
+//ocultar divs de perguntas
+function ocultar(){
+    // const icon = document.querySelector("create");
+    const div = document.querySelector(".check");
+
+    if(div.style.display === "none"){
+        div.style.display = "block";
+    }else{
+        div.style.display = "none";
+    }
+}
 
 function getQuizzes() {
     const promise = axios.get(`${API}/quizzes`);
@@ -207,8 +312,4 @@ function home() {
 
 getQuizzes();
 
-//Avança à tela de criação do quizz
-function criarQuizz(){
-    document.querySelector("main").classList.add("hide");
-    document.querySelector(".info-quizz").classList.remove("hide");
-}
+
